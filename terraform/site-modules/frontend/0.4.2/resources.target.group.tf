aa70e13d4d5482a13ea7b031947102b1890e9e63
Mike Doty <mdoty@bitpusher.com>
2020-04-15 17:21:58 -0600

    move backend traffic to 8080

--- terraform/site-modules/frontend/0.4.2/resources.target.group.tf
+++ terraform/site-modules/frontend/0.4.2/resources.target.group.tf
@@ -2,7 +2,7 @@
 resource "aws_lb_target_group" "default" {
   ##  NAME CANNOT BE LONGER THAN 32 CHARACTERS >:{
   name                              = "${local.org-env-name}"
-  port                              = 80
+  port                              = 8080
   protocol                          = "HTTP"
   vpc_id                            = "${var.vpc_strings["id"]}"
   health_check {
@@ -11,14 +11,14 @@ resource "aws_lb_target_group" "default" {
     timeout                         = 5
     interval                        = 10
     path                            = "/index.html"
-    port                            = 80
+    port                            = 8080
   }
 }
 
 resource "aws_lb_target_group_attachment" "default" {
   count                             = "${var.instance_count}"
   target_group_arn                  = "${aws_lb_target_group.default.arn}"
-  port                              = 80
+  port                              = 8080
   target_id                         = "${module.cluster.instance_ids[count.index]}"
   ##  THIS IS REQUIRED DUE TO THE TF11 'ELEMENT' ISSUE
   lifecycle {
@@ -31,8 +31,8 @@ resource "aws_security_group_rule" "cluster_ingress_alb_80" {
   security_group_id                 = "${module.cluster.sg_id}"
   description                       = "ALLOW THE ALB TO ACCESS THE TARGET GROUP ON 80"
   type                              = "ingress"
-  from_port                         = 80
-  to_port                           = 80
+  from_port                         = 8080
+  to_port                           = 8080
   protocol                          = "tcp"
   source_security_group_id          = "${module.alb.sg_id}"
 }
