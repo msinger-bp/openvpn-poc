a7791dc8a6b3f7c524a81161d9420d3eec5dbb86
Mike Doty <mdoty@bitpusher.com>
2020-04-16 11:08:13 -0600

    cleanup

--- site-cookbooks/example-hello-world/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default['aws']['ecr']['registry_id'] = '438308380087'
-default['aws']['ecr']['region']      = 'us-east-1'
--- site-cookbooks/example-hello-world/metadata.rb
+++
@@ -1,4 +0,0 @@
-name 'example-hello-world'
-version '0.1.0'
-
-depends 'docker'
