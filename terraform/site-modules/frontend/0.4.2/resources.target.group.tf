218940a6bbfc5aea3348aa58b5337bd1a62a782e
Mike Doty <mdoty@bitpusher.com>
2020-04-21 16:42:08 -0600

    have 4 containers running

--- terraform/site-modules/frontend/0.4.2/resources.target.group.tf
+++ terraform/site-modules/frontend/0.4.2/resources.target.group.tf
@@ -15,10 +15,43 @@ resource "aws_lb_target_group" "default" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "default" {
+resource "aws_lb_target_group_attachment-8080" "default" {
   count                             = "${var.instance_count}"
   target_group_arn                  = "${aws_lb_target_group.default.arn}"
   port                              = 8080
+  target_id                         = "${module.cluster.instance_ids[count.index]}"
+  ##  THIS IS REQUIRED DUE TO THE TF11 'ELEMENT' ISSUE
+  lifecycle {
+    ignore_changes                  = [ "target_id" ]
+  }
+}
+
+resource "aws_lb_target_group_attachment-8081" "default" {
+  count                             = "${var.instance_count}"
+  target_group_arn                  = "${aws_lb_target_group.default.arn}"
+  port                              = 8081
+  target_id                         = "${module.cluster.instance_ids[count.index]}"
+  ##  THIS IS REQUIRED DUE TO THE TF11 'ELEMENT' ISSUE
+  lifecycle {
+    ignore_changes                  = [ "target_id" ]
+  }
+}
+
+resource "aws_lb_target_group_attachment-8082" "default" {
+  count                             = "${var.instance_count}"
+  target_group_arn                  = "${aws_lb_target_group.default.arn}"
+  port                              = 8082
+  target_id                         = "${module.cluster.instance_ids[count.index]}"
+  ##  THIS IS REQUIRED DUE TO THE TF11 'ELEMENT' ISSUE
+  lifecycle {
+    ignore_changes                  = [ "target_id" ]
+  }
+}
+
+resource "aws_lb_target_group_attachment-8083" "default" {
+  count                             = "${var.instance_count}"
+  target_group_arn                  = "${aws_lb_target_group.default.arn}"
+  port                              = 8083
   target_id                         = "${module.cluster.instance_ids[count.index]}"
   ##  THIS IS REQUIRED DUE TO THE TF11 'ELEMENT' ISSUE
   lifecycle {
@@ -32,7 +65,7 @@ resource "aws_security_group_rule" "cluster_ingress_alb_80" {
   description                       = "ALLOW THE ALB TO ACCESS THE TARGET GROUP ON 80"
   type                              = "ingress"
   from_port                         = 8080
-  to_port                           = 8080
+  to_port                           = 8083
   protocol                          = "tcp"
   source_security_group_id          = "${module.alb.sg_id}"
 }
