f668f2e6cc3bd9f44c11b72ccd31d74bd59c773c
Mike Doty <mdoty@bitpusher.com>
2020-05-01 17:23:02 -0600

    add mysql access from bastion

--- terraform/site-modules/aurora-mysql/0.1.0/resources.sg.tf
+++ terraform/site-modules/aurora-mysql/0.1.0/resources.sg.tf
@@ -5,3 +5,16 @@ resource "aws_security_group" "this" {
   tags                                  = "${local.tags_w_name}"
 }
 output "sg_id" { value = "${aws_security_group.this.id}" }
+
+
+#${var.vpc_strings["admin-access_sg_id"]}
+
+resource "aws_security_group_rule" "mysql-sequel-pro-access" {
+  source_security_group_id  = "${var.vpc_strings["admin-access_sg_id"]}"
+  description               = "allow mysql access from bastions for sequel-pro"
+  type                      = "ingress"
+  from_port                 = 3306
+  to_port                   = 3306
+  protocol                  = "tcp"
+  security_group_id         = "${aws_security_group.this.id}"
+}
