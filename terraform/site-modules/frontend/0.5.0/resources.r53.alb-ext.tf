0bfda202459c4299dee292d42dab52dec62e3926
Aaron Cutchin <acutchin@bitpusher.com>
2020-06-18 23:02:31 +0000

    r53 record for frontend external alb

--- terraform/site-modules/frontend/0.5.0/resources.r53.alb-ext.tf
+++ terraform/site-modules/frontend/0.5.0/resources.r53.alb-ext.tf
@@ -8,3 +8,13 @@ resource "aws_route53_record" "alb-ext_apex_alias" {
     evaluate_target_health = true
   }
 }
+resource "aws_route53_record" "alb-ext_simple" {
+  zone_id = "${var.base_strings["public_subdomain_id"]}"
+  name    = "${var.name}.${var.base_strings["public_subdomain_name"]}"
+  type    = "A"
+  alias {
+    name                   = "${module.alb-ext.alb_dns_name}"
+    zone_id                = "${module.alb-ext.alb_zone_id}"
+    evaluate_target_health = true
+  }
+}
