1dfcf4f42647cabd1315b1c37e8736c9d44dc503
Mike Doty <mdoty@bitpusher.com>
2020-06-18 14:25:29 -0600

    add demo env

---
+++ terraform/env/demo/module.frontend.tf
@@ -0,0 +1,30 @@
+##  FRONTEND
+
+module "frontend" {
+  source                      = "../../site-modules/frontend/0.4.3"
+  name                        = "frontend"
+  ##  ALB
+  alb_subnet_group_octet      = "${var.subnet_group_octets["frontend_alb"]}"
+  ##  INSTANCE CLUSTER
+  instance_subnet_group_octet = "${var.subnet_group_octets["frontend_http"]}"
+  placement_group_strategy    = "spread"
+  instance_count              = "3"
+  instance_type               = "t3.medium"
+  chef_role                   = "frontend_http"
+  ##  OUTPUTS FROM REQUIRED MODULES
+  env_strings                 = "${local.env_strings}"
+  base_strings                = "${local.base_strings}"
+  vpc_strings                 = "${local.vpc-main_strings}"
+  vpc_lists                   = "${local.vpc-main_lists}"
+  az_count                    = "${var.vpc-main_az_count}"
+  chef_strings                = "${local.chef_strings}"
+  chef_lists                  = "${local.chef_lists}"
+  chef_iam_policy_count       = "${module.chef.iam_policy_count}"
+  terraform_strings           = "${local.terraform_strings}"
+  tags                        = "${local.tags}"
+  ecr_arn                     = "arn:aws:ecr:us-west-2:695990525005:repository/frontend"
+  db-main_sg                  = "${module.maindb.master_sg_id}"
+}
+
+output "frontend_alb_public_cname"  { value = "${module.frontend.alb_public_cname}" }
+output "frontend_internal_cnames"   { value = "${module.frontend.internal_cnames}" }
---
+++ terraform/env/demo/module.maindb.tf
@@ -0,0 +1,35 @@
+module "maindb" {
+  source                  = "../../site-modules/rds-mysql/0.1.0"
+  name                    = "maindb"
+  replica_count           = "0"
+  master_multi_az         = "true"
+  subnet_group_octet      = "${var.subnet_group_octets["maindb"]}"
+  ##  FOR VERSION COMPATIBILITY, CONSULT https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.DBInstanceClass.html
+  engine_major_version    = "5.7"
+  engine_minor_version    = "22"
+  instance_class          = "db.t3.small"
+  ##  FOR STORAGE COMPATIBILITY, CONSULT: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_Storage.html
+  allocated_storage       = "100"
+  max_allocated_storage   = "200"
+  storage_type            = "gp2"
+  database_name           = "app"
+  admin_username          = "admin"
+  admin_password          = "password123"
+  backup_retention_period = "32"
+  backup_window           = "09:45-10:20" ##  AT LEAST 30 MINUTES
+  maintenance_window      = "fri:14:45-fri:16:45"
+  parameters              = [
+    { name = "log_bin_trust_function_creators", value = "1" }
+  ]
+  env_strings             = "${local.env_strings}"
+  base_strings            = "${local.base_strings}"
+  vpc_strings             = "${local.vpc-main_strings}"
+  vpc_lists               = "${local.vpc-main_lists}"
+  az_count                = "${var.vpc-main_az_count}"
+  tags                    = "${local.tags}"
+}
+output "maindb_admin_password" { value = "${module.maindb.admin_password}"  }
+output "maindb_admin_username" { value = "${module.maindb.admin_username}"  }
+output "maindb_id"             { value = "${module.maindb.master_id}"       }
+output "maindb_endpoint"       { value = "${module.maindb.master_endpoint}" }
+
