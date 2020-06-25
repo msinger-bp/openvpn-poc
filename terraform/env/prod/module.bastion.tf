94ea215f79b87a9fd51ce7fdf2681694c8786fe5
Mike Doty <mdoty@bitpusher.com>
2020-06-25 15:11:41 -0600

    new prod env

---
+++ terraform/env/prod/module.bastion.tf
@@ -0,0 +1,25 @@
+##  BASTIONS
+##
+##  FOR ADMISTRATIVE SSH ACCESS
+
+module "bastion" {
+  source                        = "../../site-modules/bastion/0.3.1"
+  name                          = "bastion"
+  subnet_group_octet            = "${var.subnet_group_octets["bastion"]}"
+  #instance_count               = "${var.vpc-main_az_count}" ##  ONE PER AZ
+  instance_count                = "1"
+  instance_type                 = "t3.small"
+  base_strings                  = "${local.base_strings}"
+  env_strings                   = "${local.env_strings}"
+  chef_strings                  = "${local.chef_strings}"
+  chef_lists                    = "${local.chef_lists}"
+  chef_iam_policy_count         = "${module.chef.iam_policy_count}"
+  terraform_strings             = "${local.terraform_strings}"
+  vpc_strings                   = "${local.vpc-main_strings}"
+  vpc_lists                     = "${local.vpc-main_lists}"
+  az_count                      = "${var.vpc-main_az_count}"
+  tags                          = "${local.tags}"
+}
+output "bastion_public_cname"   { value = "${module.bastion.public_cnames[0]}" }
+output "bastion_public_cnames"  { value = "${module.bastion.public_cnames}" }
+output "bastion_internal_cnames"  { value = "${module.bastion.internal_cnames}" }
---
+++ terraform/env/prod/module.frontend.tf
@@ -0,0 +1,31 @@
+##  FRONTEND
+
+module "frontend" {
+  source                      = "../../site-modules/frontend/0.5.0"
+  name                        = "frontend"
+  ##  ALB
+  alb-ext_subnet_group_octet  = "${var.subnet_group_octets["frontend_alb-ext"]}"
+  alb-int_subnet_group_octet  = "${var.subnet_group_octets["frontend_alb-int"]}"
+  ##  INSTANCE CLUSTER
+  instance_subnet_group_octet = "${var.subnet_group_octets["frontend_instances"]}"
+  placement_group_strategy    = "spread"
+  instance_count              = "1"
+  instance_type               = "t3.large"
+  chef_role                   = "frontend_http"
+  ##  OUTPUTS FROM REQUIRED MODULES
+  env_strings                 = "${local.env_strings}"
+  base_strings                = "${local.base_strings}"
+  vpc_strings                 = "${local.vpc-main_strings}"
+  vpc_lists                   = "${local.vpc-main_lists}"
+  az_count                    = "${var.vpc-main_az_count}"
+  chef_strings                = "${local.chef_strings}"
+  chef_lists                  = "${local.chef_lists}"
+  chef_iam_policy_count       = "${module.chef.iam_policy_count}"
+  terraform_strings           = "${local.terraform_strings}"
+  tags                        = "${local.tags}"
+  ecr_arn                     = "arn:aws:ecr:us-west-2:695990525005:repository/frontend"
+  db-main_sg                  = "${module.maindb.master_sg_id}"
+}
+
+output "frontend_alb-ext_public_cname"  { value = "${module.frontend.alb-ext_public_cname}" }
+output "frontend_internal_cnames"       { value = "${module.frontend.internal_cnames}" }
