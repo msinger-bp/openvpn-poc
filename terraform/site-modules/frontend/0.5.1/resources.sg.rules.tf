2bb2e0fe6319d65e4cea4243d4585509f7d58416
Mike Doty <mdoty@bitpusher.com>
2020-06-25 16:29:29 -0600

    add additional SAN

---
+++ terraform/site-modules/frontend/0.5.1/resources.sg.rules.tf
@@ -0,0 +1,9 @@
+resource "aws_security_group_rule" "frontend-db-main-3306" {
+  security_group_id         = "${var.db-main_sg}"
+  description               = "frontend to db app"
+  type                      = "ingress"
+  from_port                 = 3306
+  to_port                   = 3306
+  protocol                  = "tcp"
+  source_security_group_id  = "${module.cluster.sg_id}"
+}
