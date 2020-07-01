faa8fd98cd75a0a36928165070420668bc74fc0a
Mike Doty <mdoty@bitpusher.com>
2020-07-01 13:02:58 -0600

    set env vars

---
+++ environments/stage/80-envvars.rb
@@ -0,0 +1,12 @@
+default['acadience-frontend']['env']['IMPORT_ROSTER_CHUNK_SIZE_SCHOOLS']=0
+default['acadience-frontend']['env']['IMPORT_ROSTER_CHUNK_SIZE_CLASSES']=0
+default['acadience-frontend']['env']['IMPORT_ROSTER_CHUNK_SIZE_STAFF']=100
+default['acadience-frontend']['env']['IMPORT_ROSTER_CHUNK_SIZE_STAFF_ENROLLMENTS']=1000
+default['acadience-frontend']['env']['IMPORT_ROSTER_CHUNK_SIZE_STUDENTS']=100
+default['acadience-frontend']['env']['IMPORT_ROSTER_CHUNK_SIZE_STUDENT_ENROLLMENTS']=1000
+default['acadience-frontend']['env']['SMTP_USER']='AKIA2EDCETRG2LQSWRDN'
+default['acadience-frontend']['env']['SMTP_PASS']='BCN5EDRQRne+FKHC5hYcN3gh2CGcv8pUbBqN0GMaOpyN'
+default['acadience-frontend']['env']['SMTP_HOST']='email-smtp.us-west-2.amazonaws.com'
+default['acadience-frontend']['env']['SMTP_POST']=587
+default['acadience-frontend']['env']['BASE_URL']='https://stage.alo.acadiencelearning.org'
+default['acadience-frontend']['env']['API_SHARED_SECRET']='wD@Derb240bdffn38MN(sacvessfgw_stage'
