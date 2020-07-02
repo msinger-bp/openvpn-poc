bd468fc7603154128f57fb7229a65355ab9d037d
Mike Doty <mdoty@bitpusher.com>
2020-07-02 15:54:56 -0600

    monitoring TF outputs

--- terraform/env/prod/outputs.instance.lists.tf
+++ terraform/env/prod/outputs.instance.lists.tf
@@ -1,6 +1,13 @@
 output "all_instances" { value = "${ concat(
   "${module.bastion.internal_cnames}",
+  "${module.frontend.internal_cnames}",
+  "${module.monitoring.internal_cnames}",
 ) }" }
 output "monitored_instances" { value = "${ concat(
   "${module.bastion.internal_cnames}",
+  "${module.frontend.internal_cnames}",
+) }" }
+output "docker_instances" { value = "${ concat(
+  "${module.frontend.internal_cnames}",
+  "${module.monitoring.internal_cnames}",
 ) }" }
