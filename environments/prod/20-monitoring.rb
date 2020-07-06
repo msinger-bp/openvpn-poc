b24615ee9c5e25508944fdbc5f5399ed41bad886
Mike Doty <mdoty@bitpusher.com>
2020-07-06 16:59:22 -0600

    fixup pagerduty

--- environments/prod/20-monitoring.rb
+++ environments/prod/20-monitoring.rb
@@ -5,4 +5,4 @@ default['acadience-monitoring']['internal_domain']='prod.alo.acadience.internal'
 default['nagios']['pagerduty']['key'] = 'e560326551cb44b7bb1088392b24e728'
 default['nagios']['pagerduty']['service_notification_options'] = 'n'
 default['nagios']['pagerduty']['host_notification_options'] = 'n'
-default['nagios']['pagerduty']['contactgroups'] = []
+default['nagios']['pagerduty']['contactgroups'] = [ 'admins' ]
