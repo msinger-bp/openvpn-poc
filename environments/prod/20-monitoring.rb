fce5ac449ee1c01dd80e00d77d986aafd01e755c
Mike Doty <mdoty@bitpusher.com>
2020-07-06 15:37:03 -0600

    set PD key

--- environments/prod/20-monitoring.rb
+++ environments/prod/20-monitoring.rb
@@ -2,3 +2,7 @@ default['acadience-monitoring']['critical_email']='ops@bitpusher.com'
 default['acadience-monitoring']['warning_email']='acadience-ops@bitpusher.com'
 default['acadience-monitoring']['internal_domain']='prod.alo.acadience.internal'
 
+default['nagios']['pagerduty']['key'] = 'e560326551cb44b7bb1088392b24e728'
+default['nagios']['pagerduty']['service_notification_options'] = 'n'
+default['nagios']['pagerduty']['host_notification_options'] = 'n'
+default['nagios']['pagerduty']['contactgroups'] = []
