0d00756de6d23e833953de9dee6114d87250d1c7
Mike Doty <mdoty@bitpusher.com>
2020-07-06 18:31:23 -0600

    setup pd

--- environments/prod/20-monitoring.rb
+++ environments/prod/20-monitoring.rb
@@ -3,6 +3,7 @@ default['acadience-monitoring']['warning_email']='acadience-ops@bitpusher.com'
 default['acadience-monitoring']['internal_domain']='prod.alo.acadience.internal'
 
 default['nagios']['pagerduty']['key'] = 'e560326551cb44b7bb1088392b24e728'
-default['nagios']['pagerduty']['service_notification_options'] = 'n'
-default['nagios']['pagerduty']['host_notification_options'] = 'n'
+default['nagios']['pagerduty']['service_notification_options'] = 'u,c,r'
+default['nagios']['pagerduty']['host_notification_options'] = 'd,r'
 default['nagios']['pagerduty']['contactgroups'] = [ 'admins' ]
+
