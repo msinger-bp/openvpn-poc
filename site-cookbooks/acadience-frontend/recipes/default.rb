13743213fb6469d2cbeedd674dc3ab65b90baf53
Mike Doty <mdoty@bitpusher.com>
2020-08-21 10:58:10 -0600

    add pm2 dir

--- site-cookbooks/acadience-frontend/recipes/default.rb
+++ site-cookbooks/acadience-frontend/recipes/default.rb
@@ -71,6 +71,13 @@ directory "/srv/acadience/frontend/data" do
   action :create
 end
 
+directory "/srv/acadience/frontend/pm2" do
+  owner  node[cookbook_name]['container']['user']
+  group  node[cookbook_name]['container']['group']
+  mode   "0755"
+  action :create
+end
+
 directory "/srv/acadience/frontend/tmp" do
   owner  node[cookbook_name]['container']['user']
   group  node[cookbook_name]['container']['group']
@@ -132,7 +139,7 @@ end
     network_mode   'host'
     env            c_env
 #    log_opts       [ 'max-size=10M', 'max-file=5' ]
-    volumes        [ '/srv/acadience/frontend/config:/config', '/srv/acadience/frontend/data:/data', '/srv/acadience/frontend/tmp:/tmp' ]
+    volumes        [ '/srv/acadience/frontend/config:/config', '/srv/acadience/frontend/data:/data', '/srv/acadience/frontend/tmp:/tmp', '/srv/acadience/frontend/pm2:/app/.pm2' ]
     ro_rootfs      true
     cap_drop       [ 'CHOWN', 'DAC_OVERRIDE', 'FOWNER', 'MKNOD', 'SETGID', 'SETUID', 'SETFCAP', 'SETPCAP', 'NET_BIND_SERVICE', 'KILL' ]
     ignore_failure true
