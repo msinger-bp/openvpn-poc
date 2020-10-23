f196880d356c78128a05aa305401ac8f4d91c473
Leah Blank <lblank@bitpusher.com>
2020-10-22 20:03:20 -0700

    Attempting to make the chef-robot recipe actually create the chef-robot user, so it can be moved out of bootstrap

--- cookbooks/chef-robot/recipes/default.rb
+++ cookbooks/chef-robot/recipes/default.rb
@@ -1,3 +1,10 @@
+# chef-robot, a member of nogroup
+users_manage 'chef-robot' do
+  group_name 'nogroup'
+  action :create
+end
+
+
 cookbook_file "/home/chef-robot/run-chef-on-all-frontend-nodes-in-this-environment.sh" do
   source "run-chef-on-all-frontend-nodes-in-this-environment.sh"
   owner  'chef-robot'
