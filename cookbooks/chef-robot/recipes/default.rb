6a3fdbc68eae147fa0430c247f094edc8ea92d22
Leah Blank <lblank@bitpusher.com>
2020-10-22 20:27:18 -0700

    Gave up on attempt to make chef-robot avoid creating other users in nogroup for now; added comment

--- cookbooks/chef-robot/recipes/default.rb
+++ cookbooks/chef-robot/recipes/default.rb
@@ -1,8 +1,10 @@
 
 # Used for chef-robot, a member of nogroup
+# For now, this will create EVERY user in "nogroup".
+# (For the moment, there is only one user there anyways...
+#  ...but I want a cleaner solution for the future.)
 users_manage 'nogroup' do
   group_name 'nogroup'
-  data_bag "users::chef-robot"
   action :create
 end
 
