7948f7b3a7f29e9f215f2a3e3221cebbdbca02ea
Leah Blank <lblank@bitpusher.com>
2020-10-22 19:52:44 -0700

    Building out chef-robot cookbook - syntax fix

--- cookbooks/chef-robot/recipes/default.rb
+++ cookbooks/chef-robot/recipes/default.rb
@@ -1,5 +1,5 @@
 cookbook_file "/home/chef-robot/run-chef-on-all-frontend-nodes-in-this-environment.sh" do
-  source run-chef-on-all-frontend-nodes-in-this-environment.sh
+  source "run-chef-on-all-frontend-nodes-in-this-environment.sh"
   owner  'chef-robot'
   group  'nogroup'
   mode   0544
