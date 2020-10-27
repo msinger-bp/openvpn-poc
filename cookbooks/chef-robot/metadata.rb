d5e4b2382fa5865e6687bcf7ca9d7831c60177b8
Leah Blank <lblank@bitpusher.com>
2020-10-27 11:18:50 -0700

    Cleaned up logic for bastion hosts. Standardised filenames.

--- cookbooks/chef-robot/metadata.rb
+++ cookbooks/chef-robot/metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Leah Blank'
 maintainer_email 'lblank@bitpusher.com'
 license          'BitPusher'
 description      'Provides the chef-robot user with tools necessary to run chef across the current environment'
-version          '0.0.1'
+version          '0.0.2'
 chef_version     '>= 12.4'
 supports         'ubuntu', '>= 14.04'
 
---
+++ cookbooks/chef-robot/recipes/bastion-hosts.rb
@@ -0,0 +1,32 @@
+# Add authorized_keys file for chef-robot that can ONLY run the "run-chef-on-all-frontend-nodes-in-this-environment.sh" command.
+cookbook_file "/home/chef-robot/.ssh/authorized_keys" do
+  source "authorized_keys-for-bastion-hosts"
+  owner  'chef-robot'
+  group  'nogroup'
+  mode   0600
+  action :create
+end
+
+# Add the command that... runs chef on all frontend nodes in this environment.
+# This script does two different things. If run on a bastion host, it SSHes into
+# frontend hosts and runs itself. But if run on a frontend host, it actually runs
+# sudo chef-client.
+
+cookbook_file "/home/chef-robot/run-chef-on-all-frontend-nodes-in-this-environment.sh" do
+  source "run-chef-on-all-frontend-nodes-in-this-environment.sh"
+  owner  'chef-robot'
+  group  'nogroup'
+  mode   0544
+  action :create
+end
+
+# Bastion host needs the ability to run "sudo git pull".
+cookbook_file "/etc/sudoers.d/chef-robot-for-bastion-hosts"
+  source "chef-robot-for-bastion-hosts"
+  owner  'root'
+  group  'root'
+  mode   0440
+  action :create
+end
+
+
