e9a8c66cd2b6cac4a9b6cbde080a8a5030984dda
Leah Blank <lblank@bitpusher.com>
2020-10-27 21:58:51 -0700

    Initial creation of attributes/default.rb in chef-robot cookbook

---
+++ cookbooks/chef-robot/attributes/default.rb
@@ -0,0 +1,5 @@
+default['chef-robot']['robot-user']['user'] = 'chef-robot'
+default['chef-robot']['robot-user']['uid'] = 501
+default['chef-robot']['robot-user']['group'] = 'chef-robot'
+default['chef-robot']['robot-user']['gid'] = 501
+
