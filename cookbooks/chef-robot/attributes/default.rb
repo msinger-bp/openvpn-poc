1a3c0eef5c109e21d1b2a901af291cd7e7d57fc3
Leah Blank <lblank@bitpusher.com>
2020-10-28 18:01:26 -0700

    Changed 'robot-user' to 'robot' throughout chef-robot cookbook

--- cookbooks/chef-robot/attributes/default.rb
+++ cookbooks/chef-robot/attributes/default.rb
@@ -1,5 +1,5 @@
-default['chef-robot']['robot-user']['user'] = 'chef-robot'
-default['chef-robot']['robot-user']['uid'] = 501
-default['chef-robot']['robot-user']['group'] = 'chef-robot'
-default['chef-robot']['robot-user']['gid'] = 501
+default['chef-robot']['robot']['user'] = 'chef-robot'
+default['chef-robot']['robot']['uid'] = 501
+default['chef-robot']['robot']['group'] = 'chef-robot'
+default['chef-robot']['robot']['gid'] = 501
 
--- cookbooks/chef-robot/recipes/default.rb
+++ cookbooks/chef-robot/recipes/default.rb
@@ -2,23 +2,23 @@
 # its group exists if it doesn't already.
 #
 # Define the following parameters in attributes/default.rb:
-# default['chef-robot']['robot-user']['user']
-# default['chef-robot']['robot-user']['uid']
-# default['chef-robot']['robot-user']['group']
-# default['chef-robot']['robot-user']['gid']
+# default['chef-robot']['robot']['user']
+# default['chef-robot']['robot']['uid']
+# default['chef-robot']['robot']['group']
+# default['chef-robot']['robot']['gid']
 #
 # This code is based on a similar pattern found in:
 # acadience-infra/site-cookbooks/acadience-frontend/default.rb
 
-group node[cookbook_name]['robot-user']['group'] do
-  gid    node[cookbook_name]['robot-user']['gid']
+group node[cookbook_name]['robot']['group'] do
+  gid    node[cookbook_name]['robot']['gid']
   system true
   action :create
 end
 
-user node[cookbook_name]['robot-user']['user'] do
-  uid     node[cookbook_name]['robot-user']['uid']
-  gid     node[cookbook_name]['robot-user']['gid']
+user node[cookbook_name]['robot']['user'] do
+  uid     node[cookbook_name]['robot']['uid']
+  gid     node[cookbook_name]['robot']['gid']
   comment "Chef-Robot user created by chef-robot Chef cookbook"
   system  true
   action  :create
