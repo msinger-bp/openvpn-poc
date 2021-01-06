a6a156ce50dcda36db85e4b954a22f08131e07ed
Mike Doty <mdoty@bitpusher.com>
2021-01-06 15:19:01 -0700

    add envflag

--- environments/dev/20-envflags.rb
+++ environments/dev/20-envflags.rb
@@ -2,4 +2,5 @@ default['env_flags']['devs_have_sudo'] = true
 default['env_flags']['is_production']  = false
 default['env_flags']['auto_migrate']   = true
 default['env_flags']['travisci_deployment'] = true
+default['env_flags']['socketio_redis'] = true
 
