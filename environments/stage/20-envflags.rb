3dd074aaa0e769a439e5dbe734d34f68f6e34579
Mike Doty <mdoty@bitpusher.com>
2021-01-07 12:51:11 -0700

    readd wss hack

--- environments/stage/20-envflags.rb
+++ environments/stage/20-envflags.rb
@@ -2,5 +2,5 @@ default['env_flags']['devs_have_sudo'] = false
 default['env_flags']['is_production']  = false
 default['env_flags']['auto_migrate']   = false
 default['env_flags']['travisci_deployment'] = false
-default['env_flags']['socketio_redis'] = true
+default['env_flags']['socketio_redis'] = false
 
